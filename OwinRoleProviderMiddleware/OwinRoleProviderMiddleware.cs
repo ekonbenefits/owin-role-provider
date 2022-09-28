b81b95712e2871c1044f0618c19c187230cdd4df
tycrawford <31459553+tycrawford@users.noreply.github.com>
2022-09-28 10:20:05 -0500

    use already defined principal in getting user
    Co-authored-by: Jay Tuley <jay@tuley.name>

--- OwinRoleProviderMiddleware/OwinRoleProviderMiddleware.cs
+++ OwinRoleProviderMiddleware/OwinRoleProviderMiddleware.cs
@@ -18,7 +18,7 @@ namespace OwinRoleProviderMiddleware
         public override async Task Invoke( IOwinContext context )
         {
             var principal = context.Request.User;
-            var user = context.Request.User.Identity as ClaimsIdentity;
+            var user = principal.Identity as ClaimsIdentity;
             if ( user != null && user.IsAuthenticated )
             {
                 var roles = await _provider.GetRolesForUser( principal );
