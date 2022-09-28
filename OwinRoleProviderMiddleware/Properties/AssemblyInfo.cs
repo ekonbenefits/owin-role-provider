970f016493092df756398b382c4e49bda5a8351a
jbtule <jay.tuley@ekonbenefits.com>
2022-09-28 10:56:06 -0500

    Migrate to newer csproj

--- OwinRoleProviderMiddleware/Properties/AssemblyInfo.cs
+++ OwinRoleProviderMiddleware/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("OwinRoleProviderMiddleware")]
-[assembly: AssemblyDescription( "Role Provider middleware for Owin" )]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany( "Sainsbury's Supermarkets Ltd" )]
-[assembly: AssemblyProduct("OwinRoleProviderMiddleware")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -20,16 +8,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("67def1ad-2771-4c73-a2c1-d648cb1c010f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
